c9f7f396b8cf6d6d65b8715dbcad8c646a2b83d9
Simona Iskander <iskandersimona@gmail.com>
2017-10-01 15:23:57 +0000

    new content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/show' , to: 'todo#show'
+  get 'todo/new', to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
   get 'todo/show/:id',to: 'todo#show'
 end
