6f68ade7245e6b14e09cd88d87c356a983870102
Simona Iskander <iskandersimona@gmail.com>
2017-10-01 18:29:49 +0000

    new content

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'todo/new', to: 'todo#create'
   get 'todo/show/:id',to: 'todo#show'
   get 'todo/edit/:id', to: 'todo#edit'
-  put 'todo/update/:id', to: 'todo#update'
-  put 'todo/destroy/:id', to: 'todo#destroy'
+  get 'todo/update/:id', to: 'todo#update'
+  get 'todo/destroy/:id', to: 'todo#destroy'
 end
