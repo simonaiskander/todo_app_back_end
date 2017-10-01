133ec441e40c5b7e207bf3e5e3663dc82500a7b2
Simona Iskander <iskandersimona@gmail.com>
2017-10-01 16:31:20 +0000

    new content

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -14,5 +14,20 @@ class TodoController < ApplicationController
   t.pomodoro_estimate = params['pomodoro_estimate']
   t.save
   redirect_to "/todo/show/#{ t.id }"
-  end
+ end
+ def edit
+ @todo = Todo.find_by_id(params[:id])
+ end
+ def update
+  t = Todo.find_by_id(params['id'])
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro-estimate']
+  t.save
+  redirect_to "/todo/show/#{t.id}"
+ end
+ def destroy
+  t = Todo.find_by_id(params[:id])
+  t.destroy
+  redirect_to "/todo/index"
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/show' , to: 'todo#show'
   get 'todo/new', to: 'todo#new'
-  get 'todo/create', to: 'todo#create'
+  post 'todo/new', to: 'todo#create'
   get 'todo/show/:id',to: 'todo#show'
+  get 'todo/edit/:id', to: 'todo#edit'
+  put 'todo/update/:id', to: 'todo#update'
+  put 'todo/destroy/:id', to: 'todo#destroy'
 end
